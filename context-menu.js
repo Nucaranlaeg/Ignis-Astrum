22e4d84c4bc765b938602cccb82627affb87b0ea
Nucaranlaeg <bennerguy@gmail.com>
2018-02-22 23:35:39 -0700

    Add logic to determine which hex is clicked on.

--- context-menu.js
+++ context-menu.js
@@ -17,6 +17,11 @@ function initializeContextMenu() {
 document.addEventListener("DOMContentLoaded", initializeContextMenu, {once: true});
 
 window.oncontextmenu = function (event) {
+	let targetHex = findHex(event.clientX, event.clientY);
+	if (!targetHex) {
+		event.preventDefault();
+		return;
+	}
 	let target = contextMenuMask.all; // TODO: determine what kind of item was clicked on
 	contextMenu.classList.add("active");
 	contextMenu.style.top = event.clientY + "px";
@@ -42,4 +47,42 @@ document.addEventListener("keyup", (event) => {
 
 function preventClose(event) {
 	event.stopPropagation();
+}
+
+function findHex(x, y) {
+	// If code is encapsulated, change references to map.
+	// Can't cache this, as it might change when panning.
+	let rows = [...map.children],
+		rowNum = null,
+		hexes = null,
+		rowOffset = null;
+	x += map.scrollLeft;
+	y += map.scrollTop;
+	// Remove the edge case where the click is above the first row.
+	if (y < 122) return null;
+	// Each row is 120 pixels, and the flat part is 80 pixels tall.
+	// 122 is where the flat bit of the first hex row starts.
+	// If it's in that band, we know exactly which row it's in - otherwise it's in one of two.
+	rowNum = Math.floor((y - 122) / 120);
+	if ((y - 122) % 120 > 80) {
+		console.log((y - 202) % 120, Math.abs((x - 29 - ((rowNum + 1 % 2) * 66)) % 131 - 66) / (66 / 40));
+		if (((y - 202) % 120) < Math.abs((x - 29 - ((rowNum + 1 % 2) * 66)) % 131 - 66) / (66 / 40)) {
+			// We're good, nothing to see here, but this makes the math easier.
+		} else if (((y - 202) % 120) > Math.abs((x - 29 - ((rowNum + 1 % 2) * 66)) % 131 - 66) / (131 / 40)) {
+			rowNum++;
+		} else {
+			return null;
+		}
+	}
+	hexes = [...rows[rowNum].children];
+	if (rowNum % 2) { // Odd rows have an offset of 95 pixels.
+		rowOffset = 95;
+	} else { // Even rows have an offset of 29 pixels
+		rowOffset = 29;
+	}
+	// Remove the edge case where the click is before the first hex.
+	if (x < rowOffset) return null;
+	// Hexes are 131 pixels wide, but from 127-131 the click is between hexes.
+	if ((x - rowOffset) % 131 >= 127) return null;
+	return hexes[Math.floor((x - rowOffset) / 131)];
 }
