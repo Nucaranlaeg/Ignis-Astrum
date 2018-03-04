43a576bbb1bec8ba29d367930c99dd955adf778d
Nucaranlaeg <bennerguy@gmail.com>
2018-03-03 22:00:35 -0700

    Add namespaces; Detect objects in a hex.

--- context-menu.js
+++ context-menu.js
@@ -1,88 +1,119 @@
-var contextMenu;
-const menuItems = [];
-const contextMenuMask = {
-	ship: [true, true, true],
-	fleet: [false, true, true],
-	hex: [false, true, false],
-	all: [true, true, true]
-}
+"use strict";
 
-function initializeContextMenu() {
-	contextMenu = document.getElementById("context-menu");
-	menuItems[0] = document.getElementById("context-target-priority");
-	menuItems[1] = document.getElementById("context-supply-grid");
-	menuItems[2] = document.getElementById("context-repair");
-}
-
-document.addEventListener("DOMContentLoaded", initializeContextMenu, {once: true});
-
-window.oncontextmenu = function (event) {
-	let targetHex = findHex(event.clientX, event.clientY);
-	if (!targetHex) {
-		event.preventDefault();
-		return;
+var ContextMenu = {};
+(function() {
+	var contextMenu;
+	const menuItems = [];
+	const contextMenuMask = {
+		friendlyShip: [false, true, true],
+		enemyShip: [true, false, false],
+		friendlyFleet: [false, true, true],
+		enemyFleet: [false, false, false],
+		friendlyBase: [false, true, true],
+		enemyBase: [true, false, false],
+		hex: [false, true, false],
+		all: [true, true, true]
 	}
-	let target = contextMenuMask.all; // TODO: determine what kind of item was clicked on
-	contextMenu.classList.add("active");
-	contextMenu.style.top = event.clientY + "px";
-	contextMenu.style.left = event.clientX + "px";
-	menuItems.forEach((item, index) => {
-		if (!item) return;
-		if (target[index]) {
-			item.classList.add("active");
-		} else {
-			item.classList.remove("active");
+
+	function initializeContextMenu() {
+		contextMenu = document.getElementById("context-menu");
+		menuItems[0] = document.getElementById("context-target-priority");
+		menuItems[1] = document.getElementById("context-supply-grid");
+		menuItems[2] = document.getElementById("context-repair");
+	}
+
+	document.addEventListener("DOMContentLoaded", initializeContextMenu, {once: true});
+
+	function loadContextMenu(event) {
+		let targetHex = this.findHex(event.clientX, event.clientY);
+		if (!targetHex) {
+			event.preventDefault();
+			return;
 		}
+		let target = this.findItem(targetHex, event.clientX, event.clientY);
+		let targetMenu = null;
+		if (target === undefined) {
+			targetMenu = contextMenuMask.hex;
+		} else if (target.classList.contains("base")) {
+			targetMenu = contextMenuMask.friendlyBase;
+			// targetMenu = contextMenuMask[(target.classList.contains("player" + playerId) ? "friendly" : "enemy") + "Base"];
+		} else if (target.classList.contains("ship")) {
+			targetMenu = contextMenuMask.friendlyShip;
+			// targetMenu = contextMenuMask[(target.classList.contains("player" + playerId) ? "friendly" : "enemy") + "Ship"];
+		} else if (target.classList.contains("ship")) {
+			targetMenu = contextMenuMask.friendlyFleet;
+			// targetMenu = contextMenuMask[(target.classList.contains("player" + playerId) ? "friendly" : "enemy") + "Fleet"];
+		} else {
+			return;
+		}
+		contextMenu.classList.add("active");
+		contextMenu.style.top = event.clientY + "px";
+		contextMenu.style.left = event.clientX + "px";
+		menuItems.forEach((item, index) => {
+			if (!item) return;
+			if (targetMenu[index]) {
+				item.classList.add("active");
+			} else {
+				item.classList.remove("active");
+			}
+		});
+		return false;
+	}
+	window.oncontextmenu = loadContextMenu.bind(this);
+
+	document.addEventListener("click", () => {
+		contextMenu.classList.remove("active");
 	});
-	return false;
-}
 
-document.addEventListener("click", () => {
-	contextMenu.classList.remove("active");
-});
+	document.addEventListener("keyup", (event) => {
+		if (event.keyCode === 27) contextMenu.classList.remove("active");
+	});
 
-document.addEventListener("keyup", (event) => {
-	if (event.keyCode === 27) contextMenu.classList.remove("active");
-});
+	this.preventClose = function(event) {
+		event.stopPropagation();
+	};
 
-function preventClose(event) {
-	event.stopPropagation();
-}
+	this.findItem = function(targetHex, x, y) {
+		return [...targetHex.children].find(item => {
+			let target = item.getBoundingClientRect();
+			return (x >= target.left && x <= target.right && y >= target.top && y <= target.bottom);
+		});
+	};
 
-function findHex(x, y) {
-	// If code is encapsulated, change references to map.
-	// Can't cache this, as it might change when panning.
-	let rows = [...map.children],
-		rowNum = null,
-		hexes = null,
-		rowOffset = null;
-	x += map.scrollLeft;
-	y += map.scrollTop;
-	// Remove the edge case where the click is above the first row.
-	if (y < 122) return null;
-	// Each row is 120 pixels, and the flat part is 80 pixels tall.
-	// 122 is where the flat bit of the first hex row starts.
-	// If it's in that band, we know exactly which row it's in - otherwise it's in one of two.
-	rowNum = Math.floor((y - 122) / 120);
-	if ((y - 122) % 120 > 80) {
-		console.log((y - 202) % 120, Math.abs((x - 29 - ((rowNum + 1 % 2) * 66)) % 131 - 66) / (66 / 40));
-		if (((y - 202) % 120) < Math.abs((x - 29 - ((rowNum + 1 % 2) * 66)) % 131 - 66) / (66 / 40)) {
-			// We're good, nothing to see here, but this makes the math easier.
-		} else if (((y - 202) % 120) > Math.abs((x - 29 - ((rowNum + 1 % 2) * 66)) % 131 - 66) / (131 / 40)) {
-			rowNum++;
-		} else {
-			return null;
+	this.findHex = function(x, y) {
+		// If code is encapsulated, change references to map.
+		// Can't cache this, as it might change when panning.
+		let rows = [...map.children],
+			rowNum = null,
+			hexes = null,
+			rowOffset = null;
+		x += map.scrollLeft;
+		y += map.scrollTop;
+		// Remove the edge case where the click is above the first row.
+		if (y < 122) return null;
+		// Each row is 120 pixels, and the flat part is 80 pixels tall.
+		// 122 is where the flat bit of the first hex row starts.
+		// If it's in that band, we know exactly which row it's in - otherwise it's in one of two.
+		rowNum = Math.floor((y - 122) / 120);
+		if ((y - 122) % 120 > 80) {
+			if (((y - 202) % 120) < Math.abs((x - 29 - ((rowNum + 1 % 2) * 66)) % 131 - 66) / (66 / 40)) {
+				// We're good, nothing to see here, but this makes the math easier.
+			} else if (((y - 202) % 120) > Math.abs((x - 29 - ((rowNum + 1 % 2) * 66)) % 131 - 66) / (131 / 40)) {
+				rowNum++;
+			} else {
+				return null;
+			}
 		}
-	}
-	hexes = [...rows[rowNum].children];
-	if (rowNum % 2) { // Odd rows have an offset of 95 pixels.
-		rowOffset = 95;
-	} else { // Even rows have an offset of 29 pixels
-		rowOffset = 29;
-	}
-	// Remove the edge case where the click is before the first hex.
-	if (x < rowOffset) return null;
-	// Hexes are 131 pixels wide, but from 127-131 the click is between hexes.
-	if ((x - rowOffset) % 131 >= 127) return null;
-	return hexes[Math.floor((x - rowOffset) / 131)];
-}
+		hexes = [...rows[rowNum].children];
+		if (rowNum % 2) { // Odd rows have an offset of 95 pixels.
+			rowOffset = 95;
+		} else { // Even rows have an offset of 29 pixels
+			rowOffset = 29;
+		}
+		// Remove the edge case where the click is before the first hex.
+		if (x < rowOffset) return null;
+		// Hexes are 131 pixels wide, but from 127-131 the click is between hexes.
+		if ((x - rowOffset) % 131 >= 127) return null;
+		return hexes[Math.floor((x - rowOffset) / 131)];
+	};
+}).apply(ContextMenu);
--- sidebar.js
+++ sidebar.js
@@ -1,39 +1,45 @@
-var abilities = {};
+"use strict";
 
-function initializeSidebar() {
-	detail = document.getElementById("ship-detail-template");
-	detail.removeAttribute("id");
-	
-	addShip({power: 3, currentHull: 4, maxHull: 5}, true);
-	addShip({power: 2, currentHull: 6, maxHull: 7}, false);
-}
+var Sidebar = {};
+(function() {
+	var abilities = {};
+	var detail;
 
-document.addEventListener("DOMContentLoaded", initializeSidebar, {once: true});
-
-function toggle(section) {
-	let target = document.getElementById(section);
-	if (target.style.maxHeight === "0px" || !target.style.maxHeight) {
-		target.style.maxHeight = target.scrollHeight + "px";
-	} else {
-		target.style.maxHeight = "0px";
+	function initializeSidebar() {
+		detail = document.getElementById("ship-detail-template");
+		detail.removeAttribute("id");
+		
+		addShip({power: 3, currentHull: 4, maxHull: 5}, true);
+		addShip({power: 2, currentHull: 6, maxHull: 7}, false);
 	}
-}
 
-function addShip(ship, allied) {
-	section = allied ? document.getElementById("friendly-ships-seen") : document.getElementById("enemy-ships-seen");
-	newShip = detail.cloneNode(true);
-	newShip.getElementsByClassName("power")[0].innerHTML = ship.power;
-	newShip.getElementsByClassName("current-hull")[0].innerHTML = ship.currentHull;
-	newShip.getElementsByClassName("max-hull")[0].innerHTML = ship.maxHull;
-	for (let i in ship.abilities) {
-		newShip.getElementsByClassName("ability-bar")[0].append(abilities[i]);
+	document.addEventListener("DOMContentLoaded", initializeSidebar, {once: true});
+
+	this.toggle = function(section) {
+		let target = document.getElementById(section);
+		if (target.style.maxHeight === "0px" || !target.style.maxHeight) {
+			target.style.maxHeight = target.scrollHeight + "px";
+		} else {
+			target.style.maxHeight = "0px";
+		}
+	};
+
+	function addShip(ship, allied) {
+		let section = allied ? document.getElementById("friendly-ships-seen") : document.getElementById("enemy-ships-seen");
+		let newShip = detail.cloneNode(true);
+		newShip.getElementsByClassName("power")[0].innerHTML = ship.power;
+		newShip.getElementsByClassName("current-hull")[0].innerHTML = ship.currentHull;
+		newShip.getElementsByClassName("max-hull")[0].innerHTML = ship.maxHull;
+		for (let i in ship.abilities) {
+			newShip.getElementsByClassName("ability-bar")[0].append(abilities[i]);
+		}
+		section.append(newShip);
 	}
-	section.append(newShip);
-}
 
-function readPriority(inputId) {
-	let value = document.getElementById(inputId).value;
-	// Scale value from 0.5 to 2
-	value = Math.pow(2, value / 50 - 1);
-	console.log(value);
-}
+	function readPriority(inputId) {
+		let value = document.getElementById(inputId).value;
+		// Scale value from 0.5 to 2
+		value = Math.pow(2, value / 50 - 1);
+		console.log(value);
+	}
+}).apply(Sidebar);
