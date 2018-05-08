9f2e32b3ba01c98d82eccaa9d952d6f53899aca5
Sam Benner <Sam.Benner.42@pleasantsolutions.com>
2018-05-08 12:17:18 -0600

    Selecting a hex is now possible and populates the "Selected Fleet" sidebar tab.

--- wasmFuncs.js
+++ wasmFuncs.js
@@ -16,4 +16,10 @@ var Wasm = {};
 	this.addShip = function(){
 		return Math.floor(Math.random() * 1000000);
 	}
+	this.getBase = function(id){
+		return {level: 1, power: 5, currentHull: 4, maxHull: 5, shield: 1, repair: 3, id: id, allied: true};
+	}
+	this.getShip = function(){
+		return {shipClass: 1, power: 3, currentHull: 3, maxHull: 5, shield: 1, repair: 1, id: "ship0", allied: true};
+	}
 }).apply(Wasm);
